0e40de5e3ed1ebdc81392ba28e52a8af0c2a29de
yjoh8892 <125770431+yjoh8892@users.noreply.github.com>
2023-12-06 02:47:12 +0900

    [Bronze II] Title: 벌집, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int range = 2;
+        int count = 1;
+        
+        if (N == 1) {
+            System.out.println(count);
+        } else {
+            while (range <= N) {
+                range = range + 6 * count;
+                count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
