911d959b6446865306956903b932558eb85466ff
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-01-26 21:11:29 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 216 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int V = sc.nextInt();
+        int Day = (V - B) / (A - B);
+        
+        if ((V - B) % (A - B) != 0) {
+            Day++;
+        }
+        
+        System.out.println(Day);
+    }
+}
