b0a767aeed1372f21271c3c276d4b2926551db2d
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-02-25 23:59:35 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1648 ms, Memory: 336784 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001];
+        
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+        
+        for (int i = 1; i < 10001; i++) {
+            while(arr[i] > 0) {
+				sb.append(i).append('\n');
+				arr[i]--;
+			}
+        }
+        System.out.println(sb);
+    }
+}
