3fe5b02e7a8daffae73868c8c48de618cbc5d001
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-12 17:32:48 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 284 ms, Memory: 6584 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Q2438
+{
+    static void Main(String[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+        }
+    }
+}
