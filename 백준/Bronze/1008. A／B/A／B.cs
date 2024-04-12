4d949d679c8f729763c0ca5a2433a26dedf4bf9d
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-12 10:13:31 +0900

    [Bronze V] Title: A/B, Time: 60 ms, Memory: 5512 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.cs
+++ 백준/Bronze/1008. A／B/A／B.cs
@@ -6,6 +6,9 @@ class Q1008
     {
         String[] S = Console.ReadLine().Split();
         
-        Console.WriteLine(double.Parse(S[0]) / double.Parse(S[1]));
+        double A = double.Parse(S[0]);
+        double B = double.Parse(S[1]);
+        
+        Console.WriteLine(A / B);
     }
 }
