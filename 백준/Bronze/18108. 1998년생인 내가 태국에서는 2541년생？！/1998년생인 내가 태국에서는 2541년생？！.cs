3a602139d736cd7457f76c19e820ef363855b321
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-12 09:43:11 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Q18108
+{
+    static void Main(String[] args)
+    {
+        String S = Console.ReadLine();
+        
+        Console.WriteLine(int.Parse(S) - 543);
+    }
+}
