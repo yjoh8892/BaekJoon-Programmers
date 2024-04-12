71f8852d4bfd6392a75d671f5f0448acbaea7da1
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-12 16:47:35 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 68 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Q25314
+{
+    static void Main(String[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < (N / 4); i++)
+        {
+            Console.Write("long ");
+        }
+        
+        Console.WriteLine("int");
+    }
+}
