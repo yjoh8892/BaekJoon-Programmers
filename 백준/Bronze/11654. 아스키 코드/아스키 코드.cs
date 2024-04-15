2f44c844a12d4d32fb52866c680ff5224c61f484
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-15 17:50:28 +0900

    [Bronze V] Title: 아스키 코드, Time: 60 ms, Memory: 5444 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Q11654
+{
+    static void Main(String[] args)
+    {
+        String S = Console.ReadLine();
+        Char C = Char.Parse(S);
+        
+        Console.Write((byte)C);
+    }
+}
