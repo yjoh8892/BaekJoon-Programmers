2d5671e453ed20171a0499bc7d97ba831a08f979
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-15 16:33:07 +0900

    [Bronze V] Title: 문자와 문자열, Time: 68 ms, Memory: 5616 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Q27866
+{
+    static void Main(String[] args)
+    {
+        String S = Console.ReadLine();
+        int N = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(S.ElementAt(N - 1));
+    }
+}
