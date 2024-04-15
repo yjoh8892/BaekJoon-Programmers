1eb29d7e593756bb8e521d2a4186cbb77d4b0b22
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-15 10:43:03 +0900

    [Bronze II] Title: 공 바꾸기, Time: 68 ms, Memory: 5964 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Q10810
+{
+    static void Main(String[] args)
+    {
+        String[] S = Console.ReadLine().Split();
+        int N = int.Parse(S[0]);
+        int M = int.Parse(S[1]);
+        int[] basket = new int[N + 1];
+        
+        for(int i = 1; i <= N; i++)
+        {
+            basket[i] = i;
+        }
+        
+        for(int i = 0; i < M; i++)
+        {
+            String[] num = Console.ReadLine().Split();
+            int A = int.Parse(num[0]);
+            int B = int.Parse(num[1]);
+            int temp = basket[A];
+            
+            basket[A] = basket[B];
+			basket[B] = temp;
+        }
+        
+        for(int i = 1; i <= N; i++)
+        {
+            Console.Write(basket[i] + " ");
+        }
+    }
+}
