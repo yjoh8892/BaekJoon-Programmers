4721a3bd24210dae2357a662595db9ddede40af1
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-16 14:00:06 +0900

    [Bronze IV] Title: 베라의 패션, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Q15439
+{
+    static void Main(String[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(N * (N - 1));
+    }
+}
