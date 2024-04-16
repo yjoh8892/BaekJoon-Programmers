41bb0749964212c5407248c8c0fb771f64a6294e
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-16 13:53:52 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 60 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Q15894
+{
+    static void Main(String[] args)
+    {
+        long n = long.Parse(Console.ReadLine());
+        
+        Console.WriteLine(4 * n);
+    }
+}
