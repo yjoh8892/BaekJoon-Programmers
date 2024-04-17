2d0ce0ace83eae33210a0ea1a3f8468244149d48
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-17 10:55:01 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 196 ms, Memory: 17612 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -5,11 +5,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         
         String word = sc.next();
-        int mid = word.length() / 2;
         int result = 1;
         
-        for(int i = 0; i < mid; i++) {
-            if(word.charAt(i) != word.charAt(word.length() - i - 1)) {
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
                 result = 0;
             }
         }
