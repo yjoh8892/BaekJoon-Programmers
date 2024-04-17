31f0b751a8e39b2fd2f36963ca3e1be693535d3b
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-17 15:34:51 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 64 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Q24265 {
+    static void Main(String[] args)
+    {
+        long n = long.Parse(Console.ReadLine());
+        
+        Console.WriteLine(n * (n - 1) / 2);
+        Console.WriteLine(2);
+    }
+}
