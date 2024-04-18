fab9e7822bb9c02a2c31e9c682f43a98459afc25
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-18 09:27:39 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 64 ms, Memory: 5384 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Q11718
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string S = Console.ReadLine();
+            
+            Console.WriteLine(S);
+            
+            if (string.IsNullOrEmpty(S))
+            {
+                return;
+            }
+        }
+    }
+}
