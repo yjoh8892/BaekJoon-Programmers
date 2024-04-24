0a42b24041b4a6be495f016b94a8b3a2de75f7ee
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-04-24 15:42:49 +0900

    [Bronze I] Title: 최소공배수, Time: 148 ms, Memory: 5816 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Q1934
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < T; i++)
+        {
+            string[] S = Console.ReadLine().Split();
+            int A = int.Parse(S[0]);
+            int B = int.Parse(S[1]);
+            
+            Console.WriteLine(A * B / getGcd(A, B));
+        }
+    }
+    
+    public static int getGcd(int a, int b) {
+        if (b == 0)
+        {
+            return a;
+        }
+        else
+        {
+            return getGcd(b, a % b);
+        }
+    }
+}
