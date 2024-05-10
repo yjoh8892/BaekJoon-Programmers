68eaa64c72fd473c591e3353cc73b4a206d6554c
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-10 15:12:36 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 68 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Q31606
+{
+    static void Main(string[] args)
+    {
+        int X = int.Parse(Console.ReadLine());
+        int Y = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(X + Y + 3);
+    }
+}
