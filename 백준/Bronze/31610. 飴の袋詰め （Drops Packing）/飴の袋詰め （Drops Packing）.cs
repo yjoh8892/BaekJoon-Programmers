3bc771ec639851b6aa17009c022c30e1f3f3b0bc
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-10 14:59:43 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 64 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Q31610
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int C = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(A * B + C);
+    }
+}
