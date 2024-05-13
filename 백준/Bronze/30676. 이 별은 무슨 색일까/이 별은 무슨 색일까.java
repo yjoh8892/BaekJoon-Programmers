6c62e4cb513f91d22ba1c4f4bba6cd052775ff6e
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-13 14:33:57 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 212 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int lambda = sc.nextInt();
+        
+        if (lambda >= 620 && lambda <= 780) {
+            System.out.println("Red");
+        } else if (lambda >= 590 && lambda < 620) {
+            System.out.println("Orange");
+        } else if (lambda >= 570 && lambda < 590) {
+            System.out.println("Yellow");
+        } else if (lambda >= 495 && lambda < 570) {
+            System.out.println("Green");
+        } else if (lambda >= 450 && lambda < 495) {
+            System.out.println("Blue");
+        } else if (lambda >= 425 && lambda < 450) {
+            System.out.println("Indigo");
+        } else if (lambda >= 380 && lambda < 425) {
+            System.out.println("Violet");
+        }
+    }
+}
