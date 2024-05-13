d2d79c2a355fd6bf24b1f6960d3111abea1c0d0e
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-13 17:15:26 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Q30030
+{
+    static void Main(string[] args)
+    {
+        int B = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(B - B / 11);
+    }
+}
