552f5f17ea6dcf857080f1aee9ca27d93a314329
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-13 17:34:15 +0900

    [Bronze V] Title: 라면 공식, Time: 248 ms, Memory: 18128 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        for (int i = 0; i < N; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int X = sc.nextInt();
+            int W = A * (X - 1) + B;
+            
+            System.out.println(W);
+        }
+    }
+}
