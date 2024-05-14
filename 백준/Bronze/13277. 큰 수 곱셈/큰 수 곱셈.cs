2a6b16d201a4826a5a6c98c760e1146c193abaf7
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-14 16:03:20 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6440 ms, Memory: 27128 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Numerics;
+
+class Q13277
+{
+    static void Main(string[] args)
+    {
+        string[] S = Console.ReadLine().Split();
+        BigInteger A = BigInteger.Parse(S[0]);
+        BigInteger B = BigInteger.Parse(S[1]);
+        
+        Console.WriteLine(BigInteger.Multiply(A, B));
+    }
+}
