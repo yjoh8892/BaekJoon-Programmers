a3494b973c45a45eb8a08b95047d062c4477128a
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-14 12:00:04 +0900

    [Bronze V] Title: Mathematics, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Q26545
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            sum += int.Parse(Console.ReadLine());
+        }
+        
+        Console.WriteLine(sum);
+    }
+}
