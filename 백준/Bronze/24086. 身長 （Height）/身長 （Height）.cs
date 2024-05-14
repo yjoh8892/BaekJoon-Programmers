8d02de95b9e93044591028a3b0d33d7cc4593241
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-14 16:55:13 +0900

    [Bronze V] Title: 身長 (Height), Time: 64 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Q24086
+{
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int Ans = B - A;
+        
+        Console.WriteLine(Ans);     
+    }
+}
