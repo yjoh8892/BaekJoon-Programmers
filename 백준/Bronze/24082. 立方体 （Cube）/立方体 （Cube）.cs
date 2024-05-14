298f81093bbd348eba25d467f405833ad092a8e2
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-14 17:31:30 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Q24082
+{
+    static void Main(string[] args)
+    {
+        int X = int.Parse(Console.ReadLine());
+        int Ans = X * X * X;
+        
+        Console.WriteLine(Ans);
+    }
+}
