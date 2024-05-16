017ded8429ddf68edcb47c27850bf9fb4650a453
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-16 13:40:12 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 64 ms, Memory: 5436 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Q26489
+{
+    static void Main(string[] args)
+    {
+        String S;
+        int count = 0;
+        
+        while ((S = Console.ReadLine()) != null)
+        {
+            count++;
+        }
+        
+        Console.WriteLine(count);
+    }
+}
