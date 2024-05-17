bb400b7c61c93c97527a2e720c51ef16b2fb042a
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-17 13:49:48 +0900

    [Bronze V] Title: Cupcake Party, Time: 212 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int R = sc.nextInt();
+        int S = sc.nextInt();
+        int Result = R * 8 + S * 3 - 28;
+        
+        System.out.println(Result);
+    }
+}
