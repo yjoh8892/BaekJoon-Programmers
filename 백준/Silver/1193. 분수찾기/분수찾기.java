facd88601b8fb23f4ae461bc7cb5725a749d5adf
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-17 15:59:57 +0900

    [Silver V] Title: 분수찾기, Time: 232 ms, Memory: 18424 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -6,17 +6,18 @@ public class Main {
         
         int X = sc.nextInt();
         int i = 0;
-		int val = 0;
+        int val = 0;
 		
-        while(val < X) {
-			i++;
-			val += i;
-		}
-		
-		if(i % 2 == 0) {
-			System.out.println((i - (val - X)) + "/" + (i + (((val - i) - X) + 1)));
-		} else {
-			System.out.println((i + (((val - i) - X) + 1)) + "/" + (i - (val - X)));
-		}
+        while (val < X)
+        {
+            i++;
+            val += i;
+        }
+        
+        if (i % 2 == 0) {
+            System.out.println((i - (val - X)) + "/" + (i + (((val - i) - X) + 1)));
+        } else {
+            System.out.println((i + (((val - i) - X) + 1)) + "/" + (i - (val - X)));
+        }
     }
 }
