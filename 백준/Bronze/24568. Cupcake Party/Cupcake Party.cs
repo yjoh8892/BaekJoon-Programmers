8a3cffb745ac11485941174566a5a62d5d5bd6b2
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-17 13:55:36 +0900

    [Bronze V] Title: Cupcake Party, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Q24568
+{
+    static void Main(string[] args)
+    {
+        int R = int.Parse(Console.ReadLine());
+        int S = int.Parse(Console.ReadLine());
+        int Result = R * 8 + S * 3 - 28;
+        
+        Console.WriteLine(Result);
+    }
+}
