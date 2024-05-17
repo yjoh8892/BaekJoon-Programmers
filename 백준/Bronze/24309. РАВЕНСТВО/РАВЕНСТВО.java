18024d601fd28d9b62a03aa5c002eda6a1eb1e10
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-17 14:09:43 +0900

    [Bronze V] Title: РАВЕНСТВО, Time: 212 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/24309. РАВЕНСТВО/РАВЕНСТВО.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        BigInteger c = sc.nextBigInteger();
+        b = b.subtract(c);
+        b = b.divide(a);
+        
+        System.out.println(b);
+    }
+}
