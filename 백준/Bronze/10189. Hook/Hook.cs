c428063b11f314d4fb5a1cffe6ddab71f0e11490
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-22 14:49:29 +0900

    [Bronze V] Title: Hook, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Q10189
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("#  # #### #### #  #");
+        Console.WriteLine("#### #  # #  # # #");
+        Console.WriteLine("#### #  # #  # # #");
+        Console.WriteLine("#  # #### #### #  #");
+    }
+}
