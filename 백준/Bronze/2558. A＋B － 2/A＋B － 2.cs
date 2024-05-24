8c2f78cf88848f93ff9d2d7e7f1527ebfcb2e419
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-24 13:38:02 +0900

    [Bronze V] Title: A+B - 2, Time: 60 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Q2558 {
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int sum = A + B;
+        
+        Console.WriteLine(sum);
+    }
+}
