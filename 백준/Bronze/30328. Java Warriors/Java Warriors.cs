16dfd3d9cf7538167fae87a07ebdc0be5749fcb9
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-05-28 11:40:04 +0900

    [Bronze V] Title: Java Warriors, Time: 64 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Q30328
+{
+    static void Main(string[] args)
+    {
+        
+        int n = int.Parse(Console.ReadLine());
+        int sum = n * 4000;
+        
+        Console.WriteLine(sum);
+    }
+}
