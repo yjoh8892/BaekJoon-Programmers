406df4d44a9ea5a73ecd0b68fa32aacd53a9e20c
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-06-13 09:19:52 +0900

    [Bronze V] Title: CAPS, Time: 500 ms, Memory: 30976 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String S = sc.nextLine().toUpperCase();
+        
+        System.out.println(S);
+    }
+}
