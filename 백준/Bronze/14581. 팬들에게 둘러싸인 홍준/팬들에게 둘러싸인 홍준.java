a864dad043e67b6d9392af1c02477fa5f91d06a7
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-06-19 14:53:44 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 220 ms, Memory: 18220 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String S = sc.nextLine();
+        
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + S + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
