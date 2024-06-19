0fde3a1d0c5eed155ed5cc1c64ab61f3af894f27
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-06-19 15:05:15 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 60 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Q14581
+{
+    static void Main(string[] args)
+    {
+        String S = Console.ReadLine();
+        
+        Console.WriteLine(":fan::fan::fan:");
+        Console.WriteLine(":fan::" + S + "::fan:");
+        Console.WriteLine(":fan::fan::fan:");
+    }
+}
