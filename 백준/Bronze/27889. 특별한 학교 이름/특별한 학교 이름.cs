d81de8ee0b11c34310d74eb653379c07bf793452
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-06-19 16:03:12 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 60 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Q27889
+{
+    static void Main(string[] args)
+    {
+        String S = Console.ReadLine();
+        
+        if (S.Equals("NLCS"))
+        {
+            Console.WriteLine("North London Collegiate School");
+        }
+        else if (S.Equals("BHA"))
+        {
+            Console.WriteLine("Branksome Hall Asia");
+        }
+        else if (S.Equals("KIS"))
+        {
+            Console.WriteLine("Korea International School");
+        }
+        else if (S.Equals("SJA"))
+        {
+            Console.WriteLine("St. Johnsbury Academy");
+        }
+    }
+}
