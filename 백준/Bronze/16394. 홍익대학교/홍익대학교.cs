b648bdc0e9bdb56f6767e88af83c4a54088ba5d6
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-06-19 13:57:25 +0900

    [Bronze V] Title: 홍익대학교, Time: 52 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Q16394
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(N - 1946);
+    }
+}
