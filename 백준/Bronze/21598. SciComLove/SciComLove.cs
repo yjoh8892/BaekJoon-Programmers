f812c0df9d541e097e910b8434469a1f86f3f8da
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-06-19 17:23:03 +0900

    [Bronze V] Title: SciComLove, Time: 84 ms, Memory: 5484 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Q21598
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < N; i++) {
+            Console.WriteLine("SciComLove");
+        }
+    }
+}
