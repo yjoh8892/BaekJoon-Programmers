0870f4a153cf70f7a05d5d6a71c7321189752bb9
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-06-21 11:05:35 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 188 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        System.out.println(2 * (n + 1) + " " + 3 * (n + 1));
+    }
+}
