b00070d89ce420e705a9a309d22e69d48ae5d31d
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-06-21 11:55:55 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 60 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Q27331
+{
+    static void Main(String[] args)
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(A + "" + B);
+    }
+}
