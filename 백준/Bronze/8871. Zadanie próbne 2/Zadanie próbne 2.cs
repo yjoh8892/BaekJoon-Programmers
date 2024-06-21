d08535f973dfb6063cecb23dcf7ce6a6fecc7019
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-06-21 11:14:05 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 64 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Q8871 {
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(2 * (n + 1) + " " + 3 * (n + 1));
+    }
+}
