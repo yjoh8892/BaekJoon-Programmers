7cf1d96b741c2a92ed0e2aec3bd00f0ce760cece
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-06-26 14:11:06 +0900

    [Bronze V] Title: Metronome, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Q27389
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine((double)n / 4);
+    }
+}
