0eda77004cca62bb57caa42abfcd8d08ae98ff3b
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-07-10 11:11:25 +0900

    [Bronze V] Title: Rook, Time: 48 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Q2393
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("  ___  ___  ___");
+        Console.WriteLine("  | |__| |__| |");
+        Console.WriteLine("  |           |");
+        Console.WriteLine("   \\_________/");
+        Console.WriteLine("    \\_______/");
+        Console.WriteLine("     |     |");
+        Console.WriteLine("     |     |");
+        Console.WriteLine("     |     |");
+        Console.WriteLine("     |     |");
+        Console.WriteLine("     |_____|");
+        Console.WriteLine("  __/       \\__");
+        Console.WriteLine(" /             \\");
+        Console.WriteLine("/_______________\\");
+    }
+}
