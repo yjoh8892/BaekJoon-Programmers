317429f8dcb782de93e1a6e3babdec60abca98a4
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-07-11 11:37:54 +0900

    [Bronze V] Title: 웰컴, Time: 48 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Q5337
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(".  .   .");
+        Console.WriteLine("|  | _ | _. _ ._ _  _");
+        Console.WriteLine("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
