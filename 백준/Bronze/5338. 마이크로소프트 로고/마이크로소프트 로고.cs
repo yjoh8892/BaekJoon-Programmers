f355532cb982ec88388a5bfc9d44567bdcbf5e46
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-07-11 11:33:18 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 48 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Q5338
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("       _.-;;-._");
+        Console.WriteLine("'-..-'|   ||   |");
+        Console.WriteLine("'-..-'|_.-;;-._|");
+        Console.WriteLine("'-..-'|   ||   |");
+        Console.WriteLine("'-..-'|_.-''-._|");
+    }
+}
