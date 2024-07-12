3ddc6d237f69d2c5a076e47300875e358c1faee0
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-07-12 11:01:33 +0900

    [Bronze V] Title: 콜센터, Time: 48 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Q5339
+{
+    static void Main(string[] args) 
+    {
+        Console.WriteLine("     /~\\");
+        Console.WriteLine("    ( oo|");
+        Console.WriteLine("    _\\=/_");
+        Console.WriteLine("   /  _  \\");
+        Console.WriteLine("  //|/.\\|\\\\");
+        Console.WriteLine(" ||  \\ /  ||");
+        Console.WriteLine("============");
+        Console.WriteLine("|          |");
+        Console.WriteLine("|          |");
+        Console.WriteLine("|          |");
+    }
+}
