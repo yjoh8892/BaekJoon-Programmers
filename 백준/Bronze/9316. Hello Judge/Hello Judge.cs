b2c6daafa8ed53cd2204885d3b972f2f413cf06a
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-07-22 13:59:02 +0900

    [Bronze IV] Title: Hello Judge, Time: 72 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Q9316
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        
+        for (int i = 1; i <= N; i++){
+            Console.WriteLine("Hello World, Judge " + i + "!");
+        }
+    }
+}
