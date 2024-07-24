c318100aa26675efd69aceed97f4db4064240d2b
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-07-24 17:48:08 +0900

    [Bronze III] Title: A+B - 6, Time: 196 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            String str = sc.next();
+            StringTokenizer st = new StringTokenizer(str, ",");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            System.out.println(a + b);
+        }
+    }
+}
