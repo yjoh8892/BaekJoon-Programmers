21fd01bb06314008aeb18d8bab78c868c3a6fdec
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-07-25 22:02:00 +0900

    [Bronze IV] Title: Next in line, Time: 64 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Q6749
+{
+    static void Main(string[] args)
+    {
+        int Y = int.Parse(Console.ReadLine());
+        int M = int.Parse(Console.ReadLine());
+        int O = 2 * M - Y;
+        
+        Console.WriteLine(O);
+    }
+}
