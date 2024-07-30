55b07773d213536c0ddaf4df8c03382e47550002
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-07-30 22:35:39 +0900

    [Bronze V] Title: 연세대학교, Time: 60 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Q15680
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        
+        if (N == 0)
+        {
+            Console.WriteLine("YONSEI");
+        }
+        else
+        {
+            Console.WriteLine("Leading the Way to the Future");
+        }
+    }
+}
