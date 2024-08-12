ff079c10e6ced603277e1dae6340d9334a004b97
Yoo Jin Oh <125770431+yjoh8892@users.noreply.github.com>
2024-08-12 15:50:10 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 168 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        
+        if (A + B < 2 * C) {
+            System.out.println(A + B);
+        } else {
+            System.out.println((A + B) - 2 * C);
+        }
+    }
+}
